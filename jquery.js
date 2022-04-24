1343172ff41edb19047ada78da6c585e89b535ab
shivam <shivamagrahari0786@gmail.com>
2022-04-24 18:03:21 +0530

    Initial Commit

---
+++ jquery.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+  //$('p').click();
+  //click on all the p element
+  //console.log("This is click fun");
+  // there are three main type in selector in jquery
+  // 1.Element selecoter
+  // 2.id selecoter
+  // 3.class selector
+  //Element selector  : This is an example of element selector which click on all p
+  // $('p').click();
+  // ID selector this is an examle of element selector whick click on all id
+  // $('#name').click();
+  //     console.log("You click on name ");
+  // Class selector this is an examle of element selector whick click on all class
+  //   $(".first").click();
+  //   console.log("You click on First ");
+
+  // Event In jQuery
+  // MouseEvent - click, dblclick, mouseenter,mouseleave
+  // KeyboardEvent - keypress, keydown,
+  // formEvent - Submit, change , focus, blur
+  // document/window Event - load , resize,scroll,unload
+
+
+  // MouseEvent - click, dubleclick, mouseenter,
+    // $('.first').click(function(){
+    //     console.log("This is  click funcations");
+    // });
+  
+    // $('#name').dblclick(function(){
+    //     console.log("this is a mouse doble click", this);    
+    // });
+
+    // console.log("this is a duble click mouse event");
+    // $('p').mouseenter(function(){
+    //     console.log("this is a mouse enter", this);
+    // });
+
+    // demoming the on method 
+    // $('p').on({
+    //     click:function(){
+    //         console.log("Thanks for your clicking", this);
+    //     },
+    //     mouseleave:function(){
+    //         console.log("Thanks for move your mouse", this);
+    //     }
+    // })
+
+    // $('#gyab').hide( function(){
+    //     console.log("This is hide");
+    // })
+
+    // $('#gyab').show( function(){
+    //     console.log("This is show");
+    // })
+
+    //slide method 
+    // $('#gyab').slideUp(2000);
+    // console.log("this is a slideup");
+    // $('#gyab').slideDown(2000);
+    // console.log("this is a slideDown");
+    // $('#gyab').slideToggle(2000);
+    // console.log("this is a slideToggle");
+
+
+
+});
